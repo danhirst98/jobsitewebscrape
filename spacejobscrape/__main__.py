1ad72eeee6f94ede2ba310ed086583a4ee86d5de
danhirst98 <50798561+danhirst98@users.noreply.github.com>
2019-06-20 18:09:15 +0100

    Restructures

---
+++ spacejobscrape/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 30 14:15:17 2019
+
+@author: DanHirst
+"""
+from spacejobscrape import app
+
+if __name__=='__main__':
+    app.run()
+
+
---
+++ spacejobscrape/app.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import subprocess
+
+def run():
+
+    #TODO: Iterate through jobs in company scripts, running each one
+    #TODO: Run XML upload
+
+    # Deletes contents of recentXML, ready to create new one
+    recentxmlfiles = glob.glob('./spacejobscrape/recentXML/*.xml')
+    for f in recentxmlfiles:
+        os.remove(f)
+
+    companyfiles = glob.glob('./spacejobscrape/companyscripts/*.py')
+    for f in companyfiles:
+        subprocess.call('python %s' % f)
+        exit(1)
